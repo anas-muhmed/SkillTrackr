6f54dfd2bec5f7c273d02a56f46e38a24e421f09
Anas <muhammedanasoff@gmail.com>
2025-06-23 01:20:02 +0000

    rendering dummy datas

--- app/journal/JournalList.tsx
+++ app/journal/JournalList.tsx
@@ -1,8 +1,25 @@
 // This component lists all journal entries using dummy data and renders each entry using JournalCard.
 
-import { dummyEntries } from "./data";
+
 import React from "react";
 import JournalCard from "./JournalCard";
+
+
+const dummyEntries = [
+  {
+    id: 1,
+    title: "First Journal Entry",
+    content: "Today I started learning Next.js.",
+    date: "2025-06-22",
+  },
+  {
+    id: 2,
+    title: "Second Journal Entry",
+    content: "I understood CSR, SSR and hydration!",
+    date: "2025-06-23",
+  },
+];
+
 
 const JournalList = () => {
   return (
